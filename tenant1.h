388a4b2901ae7181f22b03e51d806edf640ad90e
PI03makovsky <91346352+PI03makovsky@users.noreply.github.com>
2022-01-19 04:07:45 +0700

    12lb

---
+++ tenant1.h
@@ -0,0 +1,29 @@
+#pragma once
+#pragma once
+#include <string>
+#include "date.h"
+#include "human1.h"
+#include "player.h"
+class tenant { //Класс арендатор
+
+private:
+
+	std::string name; //имя
+
+	std::string surname; //фамилия
+
+	std::string patronymic; //отчество
+
+	date tntD; //связка с классом дата
+
+public:
+
+	void init(std::string n, std::string s, std::string p, date DAYDAY1); //Метод создания
+
+	void enter(); //Метод ввода
+
+	void print(); //Метод вывода
+	
+	
+
+};
